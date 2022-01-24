48523d34291ecec30dbbd6c0636a9e0c7151a6c1
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-24 22:09:28 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+        region="us-east-1"
+}
+resource "aws_instance" "instance-1" {
+        ami="ami-08e4e35cccc6189f4"
+        instance_type="t2.micro"
+	key_name="Jenkins"
+	tags={
+		Name="Ansible"
+	}
+	
+}
