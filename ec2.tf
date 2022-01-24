19277a50790cf45c4739f6b676ed9cff22b56857
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-24 22:24:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,18 @@ resource "aws_instance" "instance-1" {
         ami="ami-08e4e35cccc6189f4"
         instance_type="t2.micro"
 	key_name="Jenkins"
+	provisioner "file" {
+	 source="index.html"
+	        destination="https://github.com/keerthy-sith/ec2-modules/blob/master/index.html"
+} 
+connection {
+        type="ssh"
+        host=self.public_ip   
+        user="ec2-user"
+        password="" 
+        private_key=file("Jenkins.pem")
+}
+
 	tags={
 		Name="Ansible"
 	}
